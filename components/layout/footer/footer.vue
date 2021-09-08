2d625b14b8fe3a4c34789c57cdbdf0145a01b3c0
Mahmoud Mostafa <mahmoud.mostafa1008@gmail.com>
2021-09-08 12:16:31 +0200

    test

---
+++ components/layout/footer/footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="footer-container">
+    <div class="container">
+      <div class="logo">
+        <NuxtLink to="/">
+          <img src="../../../assets/images/logo.svg" alt="logo">
+        </NuxtLink>
+      </div>
+      <div class="links">
+        <ul class="list-unstyled">
+              <li>
+                <NuxtLink to="/">Home</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/about-us">About US</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/services">Service</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/contact-us">Contact US</NuxtLink>
+              </li>
+        </ul>
+      </div>
+      <div class="social">
+        <i class="fab fa-facebook-f"></i>
+        <i class="fab fa-instagram"></i>
+        <i class="fab fa-twitter"></i>
+        <i class="fab fa-linkedin-in"></i>
+      </div>
+      <hr class="border"/>
+      <div class="copy">
+      <p>Copyright @ We Fly. All Right Reserved</p>
+      </div>
+    </div>
+  </div>
+</template>
